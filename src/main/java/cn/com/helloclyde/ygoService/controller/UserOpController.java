31e17f2c92fdc7ec40ecb41db36e8d3bce797787
HelloClyde <helloclyde@qq.com>
2017-03-17 20:58:12 +0800

    增加获取用户所拥有的卡和牌组的接口。

--- src/main/java/cn/com/helloclyde/ygoService/controller/UserOpController.java
+++ src/main/java/cn/com/helloclyde/ygoService/controller/UserOpController.java
@@ -7,14 +7,14 @@ import cn.com.helloclyde.ygoService.service.UserOpService;
 import cn.com.helloclyde.ygoService.utils.Security;
 import cn.com.helloclyde.ygoService.vo.LoginedUser;
 import cn.com.helloclyde.ygoService.vo.ResponseResult;
+import cn.com.helloclyde.ygoService.vo.UserPackage;
 import cn.com.helloclyde.ygoService.vo.UserVO;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by HelloClyde on 2017/3/4.
@@ -53,5 +53,33 @@ public class UserOpController {
         }
     }
 
+    @ResponseBody
+    @RequestMapping(value = "/get-cards", method = {RequestMethod.GET}, produces = "application/json;charset=UTF-8")
+    public String getCards(@RequestParam String token) {
+        try {
+            UserVO loginedUser = (UserVO) LoginedUser.get(token);
+            if (loginedUser == null) {
+                throw new Exception("未登陆");
+            }
+            return new Gson().toJson(new ResponseResult(new Gson().fromJson(loginedUser.getUser().getCards(), List.class)));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Gson().toJson(new ResponseResult(e.hashCode(), e.getMessage()));
+        }
+    }
 
+    @ResponseBody
+    @RequestMapping(value = "/get-decks", method = {RequestMethod.GET}, produces = "application/json;charset=UTF-8")
+    public String getDecks(@RequestParam String token) {
+        try {
+            UserVO loginedUser = (UserVO) LoginedUser.get(token);
+            if (loginedUser == null) {
+                throw new Exception("未登陆");
+            }
+            return new Gson().toJson(new ResponseResult(new Gson().fromJson(loginedUser.getUser().getCardPackages(), UserPackage.class)));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Gson().toJson(new ResponseResult(e.hashCode(), e.getMessage()));
+        }
+    }
 }
