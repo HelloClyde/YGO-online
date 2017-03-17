91ecd758df94ebd433832beb30c3e0152e43da9c
HelloClyde <helloclyde@qq.com>
2017-03-17 21:16:08 +0800

    现在用户所拥有的卡将进行统计和排序。

--- src/main/java/cn/com/helloclyde/ygoService/service/CardManagerService.java
+++ src/main/java/cn/com/helloclyde/ygoService/service/CardManagerService.java
@@ -11,7 +11,7 @@ import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by HelloClyde on 2017/3/3.
@@ -33,11 +33,28 @@ public class CardManagerService {
         return new CardInfo(ygodataWithBLOBses.get(0));
     }
 
-    public List<Integer> getEnableCards(){
+    public List<Integer> getEnableCards() {
         ConfigExample configExample = new ConfigExample();
         configExample.createCriteria().andKeyEqualTo("enable_cards");
         List<Config> configs = configMapper.selectByExample(configExample);
         return new Gson().fromJson(configs.get(0).getValue(), List.class);
+    }
+
+    public List<Map.Entry<Integer, Integer>> getCardsNum(List<Integer> reCards) {
+        Map<Integer, Integer> dict = new HashMap<>();
+        for (int cardId : reCards) {
+            if (dict.get(cardId) == null) {
+                dict.put(cardId, 1);
+            } else {
+                dict.put(cardId, dict.get(cardId) + 1);
+            }
+        }
+        List<Map.Entry<Integer, Integer>> resultList = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : dict.entrySet()) {
+            resultList.add(entry);
+        }
+        resultList.sort((o1, o2) -> o1.getKey().compareTo(o2.getKey()));
+        return resultList;
     }
 }
 
